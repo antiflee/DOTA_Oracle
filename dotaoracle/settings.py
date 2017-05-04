2614acc4af38b32e39c5bd4417849da874cd739a
antiflee <antiflee@gmail.com>
2017-05-04 08:05:57 -0400

    start migrating from sqlite to postgresql

--- dotaoracle/settings.py
+++ dotaoracle/settings.py
@@ -78,13 +78,19 @@ WSGI_APPLICATION = 'dotaoracle.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASE_URL =
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'dota_heroes',
+#         'USER': 'antiflee',
+#         'PASSWORD': '5329987',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
