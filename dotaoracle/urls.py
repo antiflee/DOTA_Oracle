31c39fb2bf8a5aada9365c59f9a92942a31871b3
antiflee <antiflee@gmail.com>
2017-05-12 15:05:38 -0400

    revert to add ALLOWED_HOSTS

--- dotaoracle/urls.py
+++ dotaoracle/urls.py
@@ -22,5 +22,4 @@ urlpatterns = [
     url(r'^$', homepage.views.home, name='home'),
     url(r'^winrateprediction/', include('winrateprediction.urls')),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^.well-known/acme-challenge/nZcaKFYf44ATKVkr9n6-97a7hZO6Pq2QdJReCE_HZfo/', homepage.views.letsencrypt)
 ]
