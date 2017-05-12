923244a337adafe7cc856dc5b0fe449b5a344d69
antiflee <antiflee@gmail.com>
2017-05-12 14:57:31 -0400

    Revert "revert back before letsencrypt" revert back before letsencrypt This reverts commit bbd8c217a0c88bf036fba7b9506388291f669470.

--- homepage/views (ZPC's conflicted copy 2017-05-12).py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def home(request):
-    return render(request, 'homepage/home.html')
-
-def letsencrypt(request):
-    return HttpResponse('nZcaKFYf44ATKVkr9n6-97a7hZO6Pq2QdJReCE_HZfo.ci-AlUepbk4jWBShijsxfvqkRbOAk8WJM0zSR6EZ1MY')
